17f1206478bd8d46a8e5b98635e4a00cb11425a8
akhil.ka <akhil.ka@foundingminds.com>
2023-12-31 20:33:00 +0530

    Update replace_char.py

--- Recursion/replace_char.py
+++ Recursion/replace_char.py
@@ -2,7 +2,7 @@
 Python Script to Replace Characters in a String Using Recursion
 
 This script defines a function `replaceChar` that replaces all occurrences of a specified character with another character in a string, using recursion.
-It demonstrates the use of recursion in Python for character replacement within a string and includes examples of how to use the function.
+It demonstrates the use of recursion in Python for character replacement within a string and includes various examples of how to use the function.
 
 Functions:
     replaceChar(s, a, b): Replace all occurrences of character 'a' with 'b' in string 's'.
@@ -11,10 +11,6 @@ Functions:
 def replaceChar(s, a, b):
     """
     Replace all occurrences of character 'a' with 'b' in string 's', using recursion.
-
-    The function recursively processes each character of the string. If the character matches 'a', 
-    it is replaced with 'b'; otherwise, the character remains unchanged. The base case is reached 
-    when the string is empty.
 
     Parameters:
     - s (str): The string in which the replacement is to be performed.
@@ -40,3 +36,13 @@ if __name__ == "__main__":
     # Example usage of the replaceChar function
     string_to_be_replaced = "dacdxcd"
     print(f"Replacing 'c' with 'x' in '{string_to_be_replaced}': {replaceChar(string_to_be_replaced, 'c', 'x')}")
+
+    # Additional Examples
+    example_string = "balloon"
+    print(f"Replacing 'l' with 'z' in '{example_string}': {replaceChar(example_string, 'l', 'z')}")
+
+    example_string = "abcdefgh"
+    print(f"Replacing 'a' with 'q' in '{example_string}': {replaceChar(example_string, 'a', 'q')}")
+
+    example_string = "123456789"
+    print(f"Replacing '5' with '0' in '{example_string}': {replaceChar(example_string, '5', '0')}")
